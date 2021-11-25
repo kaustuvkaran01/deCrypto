a57d0241dced33fb87e1b96154cf0b6146b0a73b
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-11-25 20:59:10 +0530

    add : Homepage basic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Landing from "./pages/Landing";
+import { Layout } from "antd";
+
+import {
+  Navbar,
+  HomePage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
 
 function App() {
-	// Write new routes by adding a route tag underneath the home route by doing something like this:
-	// <Route path="<YOUR_PATH_URL>" component={<NAME_OF_COMPONENT>}/>
-	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route path="/home" component={Home} />
-				</Switch>
-			</Router>
-		</div>
-	);
+  // Write new routes by adding a route tag underneath the home route by doing something like this:
+  // <Route path="<YOUR_PATH_URL>" component={<NAME_OF_COMPONENT>}/>
+  return (
+    <div className="App">
+      <Router>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Switch>
+                <Route exact="/" component={HomePage} />
+                <Route exact="/cryptocurrencies" component={Cryptocurrencies} />
+                <Route
+                  exact="/cryptocurrencies/:id"
+                  component={CryptoDetails}
+                />
+                <Route exact="/" component={News} />
+              </Switch>
+            </div>
+          </Layout>
+        </div>
+        <div className="footer"></div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
